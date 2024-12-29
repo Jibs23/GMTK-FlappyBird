b1403a4c54208d8641353652730c5c1e4d0785bc
Laurids <49726628+Jibs23@users.noreply.github.com>
2024-12-29 15:17:14 +0100

    Added bird animations

--- Assets/Scripts/bird.cs
+++ Assets/Scripts/bird.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using System.Collections;
 
 public class bird : MonoBehaviour
 {
+    public Animator birdAnimator;
     public Rigidbody2D myRigidbody;
     public LogicScript logic;
     public SoundEffectPlayer sound;
     public float flap_strenght = 7;
     public bool isDead = false;
     public bool offScreen = false;
+    private Coroutine flapCoroutine;
     private void Die() // This method is called when the bird dies
     {
         if (isDead) return;
@@ -29,9 +32,14 @@ public class bird : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space) == true && isDead == false)
         {
-            Debug.Log("Flap");
-            myRigidbody.linearVelocity = Vector2.up * flap_strenght;
+            if (flapCoroutine != null) /* If the bird is already flapping, stop the current flap coroutine and start a new one. */
+            {
+                StopCoroutine(flapCoroutine); /* Stop the current flap coroutine */ /*! doesen't really work, but whatever */
+            }
+            flapCoroutine = StartCoroutine(FlapCoroutine()); /* Start the flap coroutine */
             sound.PlayFlapSound();
+            myRigidbody.linearVelocity = Vector2.up * flap_strenght;
+            Debug.Log("Flap");
         }
 
         if (transform.position.y > 8 || transform.position.y < -8) // If the bird is off screen
@@ -43,6 +51,13 @@ public class bird : MonoBehaviour
             }
             Die();
         }
+    }
+        private IEnumerator FlapCoroutine() /* This method is called when the bird flaps its wings. It plays the flap animation and resets the trigger after the animation is done. */
+    {
+        birdAnimator.SetTrigger("flap");
+        yield return new WaitForSeconds(birdAnimator.GetCurrentAnimatorStateInfo(0).length); /* Wait an amount of time equal to the lenght of the animation at animation layer 0 */
+        birdAnimator.ResetTrigger("flap");
+        flapCoroutine = null; // Reset the coroutine reference when done
     }
     private void OnCollisionEnter2D(Collision2D collision)  // COLLISION DETECTION
     {
